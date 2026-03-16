a9c586e36ad53b9551d5b17dc976d50fc7412bac
vmpreciado <47041478+vmpreciado@users.noreply.github.com>
2026-03-15 22:56:19 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-kph: Koopman-Port-Hamiltonian EDMD package.
-
-Main entry point: KpHEDMD class in kph.edmd
-"""
-from .edmd import KpHEDMD
-
-__version__ = "1.0.0"
-__author__  = "Victor M. Preciado"
